8079f96b22e742c479c219928237d0bc2820c4f7
Gustavo <gustavoaaguiar2004@gmail.com>
2022-01-17 21:05:18 -0300

    17/01/2022

--- tests.py
+++ tests.py
@@ -3,24 +3,3 @@
 
 # add a system to sell the items and automatically calculate the final price and remove the right quantity of the
 # spreadsheet
-
-import secrets
-
-
-# register the user:
-
-user = str(input('Insert the username: '))
-regis = int(input('You want to:\n1-Use your password \n2-Use an auto generated password '))
-
-if regis == 1:
-    password = str(input('Insert the password: '))
-elif regis == 2:
-    special_char = '!@#$%&*-+?'
-    letters = 'abcdefghijklmnopqrstuvwxyz'
-    numbers = '0123456789'
-    alphabet = letters + letters.upper() + special_char + numbers
-    password = ''.join(secrets.choice(alphabet) for i in range(10))  # generate password with the number of
-    # digits I want
-    print(f'Your password is: {password}')
-else:
-    print('please select a valid answer')
